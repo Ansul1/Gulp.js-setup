0ca7875d5e13afe43542c30320d2607d2994be5d
Ansulz <nslprajapati@gmail.com>
2020-10-29 18:08:41 +0545

    v1

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+const livereload = require('gulp-livereload');
+const browserSync = require('browser-sync').create();
+
+//Compile SCSS to CSS
+function style() {
+  //1. Find scss file
+  return (
+    gulp
+      .src('./scss/**/*.scss')
+      //2. Passing that file to SASS compiler
+      .pipe(sass())
+      .pipe(prefix())
+      //3. Compiled CSS file save location
+      .pipe(gulp.dest('./css'))
+      .pipe(livereload())
+      .pipe(browserSync.stream())
+  );
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
